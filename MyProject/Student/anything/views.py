7dc847755cb792cb4d6a7ea505d13966fba62fbf
guruprasath1234 <guru00825@gmail.com>
2026-03-04 12:21:10 +0530

    context

--- MyProject/Student/anything/views.py
+++ MyProject/Student/anything/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, "home.html")
+    data={
+        'name': 'abc'
+    }
+    context={}
+    if request.method=='post':
+        username=request.post.get('username')
+        if username:
+            context['username']=username
+            
+    return render(request, "home.html",data)
